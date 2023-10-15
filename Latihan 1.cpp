c5a5b5a2671ed4b09a86aba28e107d7fccc549fc
reginabara <reginaabaragmail.com>
2023-10-15 19:07:46 +0800

    test1

---
+++ Latihan 1.cpp
@@ -0,0 +1,19 @@
+//membuat variabel
+double: totalNilai;
+char: nilaiHuruf;
+
+//memberi nilai awal pada variabel:
+totalNilai=0.0;
+nilaiHuruf='';
+
+if(totalNilai>=80 and totalNilai<100) then
+   nilaiHuruf='A';
+end if
+else if(totalNilai>70= and totalNilai<80) then
+   nilaiHuruf='B'
+end if
+else
+    nilaiHuruf='C';
+end else
+
+print(nilaiHuruf);
